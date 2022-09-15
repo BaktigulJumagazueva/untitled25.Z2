ef7d2f5b87a02763d7275d68261415fcb67645ec
13 <baktyguljumagazyeva@gmail.com>
2022-09-15 10:56:46 +0600

    DZ25.1

--- src/com/company/City.java
+++ src/com/company/City.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class City {
+package com.company;
+
+import com.sun.source.util.Trees;
+
+import java.util.Collections;
+import java.util.TreeSet;
+
+public class City implements Comparable<City> {
+    private int kode;
+    private String mane;
+
+    public City(int kode, String mane) {
+        this.kode = kode;
+        this.mane = mane;
+
+    }
+
+    public int getKode() {
+        return kode;
+    }
+
+    public void setKode(int kode) {
+        this.kode = kode;
+    }
+
+    public String getMane() {
+        return mane;
+    }
+
+    public void setMane(String mane) {
+        this.mane = mane;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "kode=" + kode +
+                ", mane='" + mane + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(City o) {
+
+        return this.kode - o.kode;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,26 @@
+package com.company;
+
+import java.util.*;
+
+import static java.util.Collections.reverseOrder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random random = new Random();
+        TreeSet<City> set = new TreeSet<>();
+
+        for (int i = 0; i < 15; i++) {
+            City city= new City(random.nextInt(1,1000),"City" + (i+1));
+            if (city.getKode() % 2 != 0){
+                set.add(city);
+            }
+        }
+
+        TreeSet<City> treeSetdes = (TreeSet<City>) set.descendingSet();
+        for (City i: treeSetdes) {
+            System.out.println(i);
+        }
+
+        }
     }
-}
